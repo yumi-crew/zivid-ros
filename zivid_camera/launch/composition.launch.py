29aa1f7221a56ebbabd74ce93bd783b61ea1369c
markubjo <markusbjon@gmail.com>
2020-02-19 14:18:01 +0100

    rviz

--- zivid_camera/launch/composition.launch.py
+++ zivid_camera/launch/composition.launch.py
@@ -1,12 +1,25 @@
 
 """Launch a zivid_camera component in a component container."""
+###########
+# put here by markus 
+import os
+from ament_index_python.packages import get_package_share_directory
+###########
 
 import launch
-from launch_ros.actions import ComposableNodeContainer
+from launch_ros.actions import ComposableNodeContainer, Node
 from launch_ros.descriptions import ComposableNode
 
 
+
 def generate_launch_description():
+    ######put here by markus
+    rviz_config_dir = os.path.join(get_package_share_directory(
+        'zivid_camera'), 'rviz', 'camera_view.rviz')
+    assert os.path.exists(rviz_config_dir)
+    ############
+
+
     """Generate launch description with multiple components."""
     container = ComposableNodeContainer(
         node_name='zivid_container',
@@ -23,4 +36,15 @@ def generate_launch_description():
         output='screen',
     )
 
-    return launch.LaunchDescription([container])
+
+    return launch.LaunchDescription([container,
+    #########################
+    # put here by markus    
+        Node(package='rviz2',
+            node_executable='rviz2',
+            node_name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            output='screen'
+            )
+    ########################
+    ])
